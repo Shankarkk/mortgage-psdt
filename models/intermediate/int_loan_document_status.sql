50d1bcbac0f9dafb2b6a250fe5cc7a2a9eb9588c
shankar k <mailbox869115@gmail.com>
2025-05-30 06:55:25 +0000

    commit

--- models/intermediate/int_loan_document_status.sql
+++ models/intermediate/int_loan_document_status.sql
@@ -1,45 +1,37 @@
-{{config(materialized='view')}}
+{{ config(materialized='view') }}
 
 with documents as (
     select
-        loan_id,
+        trim(loan_id) as loan_id,
         count(loan_id) as total_documents,
-        count_if(is_verified='yes')as verified_documents
-    from {{ ref('stg_documents')}}
-    group by loan_id
+        count_if(lower(trim(is_verified)) = 'yes') as verified_documents
+    from shankar_dev.my_own_dev.stg_documents
+    group by trim(loan_id)
 ),
 
 loan as (
     select 
-        loan_id, 
+        trim(loan_id) as loan_id, 
         customer_id, 
         loan_type, 
         loan_amount, 
         sanction_date, 
         branch_code
-    from {{ ref('stg_loan')}}    
-
+    from {{ ref('stg_loan') }}    
 )
-    select 
-        d.loan_id,
-        d.total_documents,
-        d.verified_documents,
-        l.customer_id, 
-        l.loan_type, 
-        l.loan_amount, 
-        l.sanction_date, 
-        l.branch_code,
-        case
-            when d.total_documents=d.verified_documents then 'fully verfied'
-            else 'pending verification'    
-        end as document_status
-    from documents d
-    left join loan l
-    on d.loan_id=l.loan_id
 
-
-
-     
-
-
-
+select 
+    d.loan_id,
+    d.total_documents,
+    d.verified_documents,
+    l.customer_id, 
+    l.loan_type, 
+    l.loan_amount, 
+    l.sanction_date, 
+    l.branch_code,
+    case
+        when d.total_documents = d.verified_documents then 'fully verified'
+        else 'pending verification'    
+    end as document_status
+from documents d
+left join loan l on d.loan_id = l.loan_id
